f53ebfe169b2135c5cf59be58b9cd99fd5672559
phoenixtree <wu_ton-g@foxmail.com>
2019-08-25 09:10:37 +0800

    start

---
+++ src/main/java/com/phoenix/poplar/PoplarApplication.java
@@ -0,0 +1,11 @@
+package com.phoenix.poplar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoplarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PoplarApplication.class, args);
+    }
+}
