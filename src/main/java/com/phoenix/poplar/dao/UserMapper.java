aa8da4bdfbc3a1d94b0758407d6176ede6330b1b
phoenixtree <wu_ton-g@foxmail.com>
2019-10-24 22:52:49 +0800

    auto-update

---
+++ src/main/java/com/phoenix/poplar/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.phoenix.poplar.dao;
+
+import com.phoenix.poplar.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/phoenix/poplar/utils/PoplarResult.java
@@ -0,0 +1,66 @@
+package com.phoenix.poplar.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+
+public class PoplarResult extends HashMap<String, Object> {
+
+    public PoplarResult() {
+        build("code", 200);
+    }
+
+    /**
+     * @Description 返回成功结果
+     * @Author 梧桐
+     */
+    public static PoplarResult ok() {
+        return new PoplarResult();
+    }
+
+    public static PoplarResult ok(String msg) {
+        PoplarResult result = new PoplarResult();
+        result.put("msg", msg);
+        return result;
+    }
+
+    public static PoplarResult ok(String code, String msg) {
+        PoplarResult result = new PoplarResult();
+        result.put("code", code);
+        result.put("msg", msg);
+        return result;
+    }
+
+    public PoplarResult ok(String code,String msg, Object data) {
+        PoplarResult result = new PoplarResult();
+        result.put("code", code);
+        result.put("msg", msg);
+        result.put("data",data);
+        return result;
+    }
+
+    /**
+     * @Description 返回错误
+     * @Author 刘俊重
+     */
+    public static PoplarResult error(int code, String msg) {
+        PoplarResult result = new PoplarResult();
+        result.put("code", code);
+        result.put("msg", msg);
+        return result;
+    }
+
+    public static PoplarResult error() {
+        return error(500, "未知错误，请稍后重试!");
+    }
+
+    public static PoplarResult error(String msg) {
+        return error(500, msg);
+    }
+
+    public PoplarResult build(String key, Object value) {
+        super.put(key, value);
+        return this;
+    }
+}
