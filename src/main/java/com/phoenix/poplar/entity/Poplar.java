6d7460d5799a837e9e4f79c92f67243715994d74
phoenixtree <wu_ton-g@foxmail.com>
2019-10-30 23:36:26 +0800

    auto-update

--- src/main/java/com/phoenix/poplar/entity/Poplar.java
+++ src/main/java/com/phoenix/poplar/entity/Poplar.java
@@ -1,8 +1,10 @@
 package com.phoenix.poplar.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class Poplar {
 
     /**
--- src/main/java/com/phoenix/poplar/entity/User.java
+++ src/main/java/com/phoenix/poplar/entity/User.java
@@ -1,12 +1,12 @@
 package com.phoenix.poplar.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class User {
     private Integer id;
-
     private String name;
-
     private Integer age;
 }
