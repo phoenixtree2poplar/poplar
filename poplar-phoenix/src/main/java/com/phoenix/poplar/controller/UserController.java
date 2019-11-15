76a9e2a8d651dbf5460f7893e041e27d15273532
phoenixtree <wu_ton-g@foxmail.com>
2019-11-15 22:37:35 +0800

    auto-update

--- poplar-phoenix/src/main/java/com/phoenix/poplar/controller/UserController.java
+++ poplar-phoenix/src/main/java/com/phoenix/poplar/controller/UserController.java
@@ -5,22 +5,23 @@ import com.phoenix.common.utils.PoplarResult;
 import com.phoenix.dao.entity.User;
 import com.phoenix.poplar.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/User")
+@RequestMapping("user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/addUser")
-    public PoplarResult insert(String user) {
+    @RequestMapping("addUser")
+    public PoplarResult insert(@RequestBody String user) {
         User insertUser = JSONObject.parseObject(user, User.class);
         int result = userService.insert(insertUser);
         if (result > 0) {
-            return PoplarResult.ok(200,"success","插入用户成功");
+            return PoplarResult.ok(200, "success", "插入用户成功");
         } else {
             return null;
         }
