141c4787b6b8b768bc8200015d9bb1fb502ad7f5
phoenixtree <wu_ton-g@foxmail.com>
2019-11-23 09:21:05 +0800

    auto-update

---
+++ poplar-phoenix/src/main/java/com/phoenix/poplar/interceptor/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.phoenix.poplar.interceptor;
+
+import com.phoenix.common.utils.PoplarResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    // 拦截异常的类型
+//    @ExceptionHandler(value = Exception.class)
+    public PoplarResult exceptionHandler(Exception e) {
+
+        log.info("------未知错误-start-----");
+        e.printStackTrace();
+        log.info("------未知错误-end-----");
+
+        // 简单返回给服务器
+        return PoplarResult.error("未知错误，请稍后重试");
+    }
+}
