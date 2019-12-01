85891691402c169f15162064784164b38b607fc2
phoenixtree <wu_ton-g@foxmail.com>
2019-12-01 09:40:45 +0800

    auto-update

--- poplar-dao/src/main/java/com/phoenix/dao/entity/User.java
+++ poplar-dao/src/main/java/com/phoenix/dao/entity/User.java
@@ -1,17 +1,43 @@
 package com.phoenix.dao.entity;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class User {
-    private String id;
+    private Integer id;
 
     private String userId;
 
     private String userName;
 
     private String userAge;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(String userAge) {
+        this.userAge = userAge == null ? null : userAge.trim();
+    }
 }
