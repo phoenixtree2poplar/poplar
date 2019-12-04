106c1f4c4456c3ff47236b2a917392f9eb830d0e
phoenixtree <wu_ton-g@foxmail.com>
2019-12-04 20:38:17 +0800

    poplar

--- poplar-dao/src/main/java/com/phoenix/dao/mapper/UserMapper.java
+++ poplar-dao/src/main/java/com/phoenix/dao/mapper/UserMapper.java
@@ -2,10 +2,9 @@ package com.phoenix.dao.mapper;
 
 import com.phoenix.dao.entity.User;
 import com.phoenix.dao.entity.UserExample;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
     long countByExample(UserExample example);
