ee0bf3ee91c8ccf9d390dfddc5318d5968f7adbd
phoenixtree <wu_ton-g@foxmail.com>
2019-12-04 20:28:04 +0800

    poplar

--- sql/poplar.sql
+++ sql/poplar.sql
@@ -1,37 +1,39 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50727
-Source Host           : localhost:3306
-Source Database       : poplar
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : localhost:3306
+ Source Schema         : poplar
 
-Target Server Type    : MYSQL
-Target Server Version : 50727
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
 
-Date: 2019-12-01 09:43:37
+ Date: 04/12/2019 20:27:17
 */
-
 CREATE DATABASE `poplar` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 use poplar;
 SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
-CREATE TABLE `t_user` (
+CREATE TABLE `t_user`  (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '唯一标示',
-  `user_id` varchar(200) DEFAULT NULL COMMENT '用户标示',
-  `user_name` varchar(200) NOT NULL COMMENT '用户名',
-  `user_age` varchar(200) DEFAULT NULL COMMENT '用户年龄',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `user_id` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户标示',
+  `user_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `user_age` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户年龄',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('1', 'phoenixtree', 'wutong', '21');
-INSERT INTO `t_user` VALUES ('2', 'poplar', 'di', '21');
+INSERT INTO `t_user` VALUES (1, 'phoenixtree', 'wutong', '21');
+INSERT INTO `t_user` VALUES (2, 'poplar', 'di', '21');
+
+SET FOREIGN_KEY_CHECKS = 1;
