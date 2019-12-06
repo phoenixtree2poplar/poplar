715e4762dd14511894b33c1ac50b98f83761124d
phoenixtree <wu_ton-g@foxmail.com>
2019-12-06 20:52:14 +0800

    auto-update

---
+++ poplar-dao/src/main/resources/db/migration/V1__poplar-init.sql
@@ -0,0 +1,42 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : localhost:3306
+ Source Schema         : poplar
+
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
+
+ Date: 04/12/2019 20:27:17
+ -- ----------------------------------------------------------------------
+ 先清空库
+ DROP DATABASE IF EXISTS poplar;
+ CREATE DATABASE poplar DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '唯一标示',
+  `user_id` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户标示',
+  `user_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `user_age` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户年龄',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES (1, 'phoenixtree', 'wutong', '21');
+INSERT INTO `t_user` VALUES (2, 'poplar', 'di', '21');
+
+SET FOREIGN_KEY_CHECKS = 1;
